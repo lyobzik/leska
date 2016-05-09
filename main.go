75d091f0641c1a5ecff885501ebd031ca5030a78
lyobzik <lyobzik@gmail.com>
2016-05-09 21:36:16 +0600

    Simple http-proxy with oxy.

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/roundrobin"
+	"github.com/vulcand/oxy/testutils"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+	Upstreams []string `short:"u" long:"upstream" required:"true"`
+	Address   string   `short:"a" long:"address" required:"true"`
+}
+
+func ParseArgs() (Config, error) {
+	config := Config{}
+	_, err := flags.Parse(&config)
+	return config, err
+}
+
+func main() {
+	config, err := ParseArgs()
+	if err != nil {
+		log.Fatalf("Cannot parse arguments")
+	}
+	forwarder, err := forward.New()
+	if err != nil {
+		log.Fatalf("Cannot create forwarder")
+	}
+
+	loadBalancer, err := roundrobin.New(forwarder)
+	if err != nil {
+		log.Fatalf("Cannot create load balancer")
+	}
+
+	for _, address := range config.Upstreams {
+		loadBalancer.UpsertServer(testutils.ParseURI(address))
+	}
+
+	server := &http.Server{
+		Addr:    config.Address,
+		Handler: loadBalancer,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Cannot start server: %v", err)
+	}
+}
