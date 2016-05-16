3f85f89659ad6097e06c55549825751f996fddfb
lyobzik <lyobzik@gmail.com>
2016-05-16 21:41:55 +0600

    Add test http server.

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"net/http"
 	"reflect"
 	"time"
+	"io/ioutil"
+	"fmt"
 )
 
 type Config struct {
@@ -39,8 +41,24 @@ func ErrorHandler(response http.ResponseWriter, request *http.Request, err error
 	response.Write([]byte(http.StatusText(statusCode)))
 }
 
+func RunTestServer(address string) {
+	http.ListenAndServe(address, http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		file, err := ioutil.TempFile("tmp_server", fmt.Sprintf("%d_", time.Now().Unix()))
+		if err != nil {
+			log.Printf("cannot save request in tmp_server: %v", err)
+		}
+		defer file.Close()
+		request.Write(file)
+		response.WriteHeader(http.StatusOK)
+	}))
+}
+
 /////////////////////////////////////////////////
 func main() {
+	go RunTestServer(":8088")
+	go RunTestServer(":8089")
+	go RunTestServer(":8090")
+
 	config, err := ParseArgs()
 	if err != nil {
 		log.Fatalf("Cannot parse arguments")
